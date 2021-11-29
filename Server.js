f8cc0c5361f6d4f01e6933d5887e5beb375e1cb2
iamsaadurrehman <saadurrehman59@gmail.com>
2021-11-30 00:15:16 +0500

    fixed backend issue

--- Server.js
+++ Server.js
@@ -6,15 +6,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/user", (req, res) => {
+  res.json({ user: "hello" });
+});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, resp) => {
     resp.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.get("/api/user", (req, res) => {
-  res.json({ user: "hello" });
-});
 
 app.listen(PORT, console.log("Started server on port:: ", PORT));
