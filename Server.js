a7eaeb6e48bfc4cc7dc2ce95d79274660c7848cf
iamsaadurrehman <saadurrehman59@gmail.com>
2021-11-30 00:03:29 +0500

    some changes

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, resp) => {
-    resp.sendFile(path.join(__dirname, "testapp", "build", "index.html"));
+    resp.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
